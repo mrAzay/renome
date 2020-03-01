9d2bc16a737e0e605c82b0d2ae807a34182079d8
mrAzay <furoxaz@gmail.com>
2020-03-01 23:39:06 +0200

    very mach all

--- gulpfile.js
+++ gulpfile.js
@@ -43,11 +43,11 @@ let gulp = require ('gulp'),
     gulp.task('style', function(){ //создаём единую библиотеку из css-стилей всех плагинов
       return gulp.src([ //указываем, где брать исходники
         'node_modules/normalize.css/normalize.css',
-        'node_modules/@fortawesome/fontawesome-free/css/all.css',
         'node_modules/slick-carousel/slick/slick.css',
         'node_modules/slick-carousel/slick/slick-theme.css',
         'node_modules/jquery.flipster/dist/jquery.flipster.css',
-        'node_modules/flatpickr/dist/flatpickr.min.css'
+        'node_modules/flatpickr/dist/flatpickr.min.css',
+        'node_modules/lightgallery/dist/css/lightgallery.css'
       ])
       .pipe(sourcemaps.init())
       .pipe(concat('libs.min.css')) //склеиваем их в один файл с указанным именем
@@ -58,11 +58,11 @@ let gulp = require ('gulp'),
 
     gulp.task('script', function(){ //аналогично поступаем с js-файлами
       return gulp.src([ //тут подключаем разные js в общую библиотеку. Отключите то, что вам не нужно.
-        'node_modules/@fortawesome/fontawesome-free/js/all.js',
         'node_modules/slick-carousel/slick/slick.js',
         'node_modules/jquery.flipster/dist/jquery.flipster.min.js',
         'node_modules/headhesive/dist/headhesive.min.js',
-        'node_modules/flatpickr/dist/flatpickr.min.js'
+        'node_modules/flatpickr/dist/flatpickr.min.js',
+        'node_modules/lightgallery/dist/js/lightgallery.js'
       ])
       .pipe(sourcemaps.init())
       .pipe(concat('libs.min.js'))
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,98 @@
 $(function () {
 
+	$('ul.product-tab__caption').on('click', 'li:not(.active)', function() {
+    $(this)
+      .addClass('product-tab__link--active').siblings().removeClass('product-tab__link--active')
+      .closest('div.product-tab__inner').find('div.product-tab__content').removeClass('product-tab__content--active').eq($(this).index()).addClass('product-tab__content--active');
+  });
+
+	$('.product__subimg').click(function (e) {
+		e.preventDefault();
+		var src = $(this).attr('src');
+		$('.product__subimg').removeClass('product__subimg--active');
+		$(this).addClass('product__subimg--active');
+
+		$('.product__img').attr('src', src);
+	});
+	let filter = $("[data-filter]");
+
+	filter.on("click", function (event) {
+		event.preventDefault();
+
+		$('.filter-item').removeClass('filter-item--active');
+		$(this).addClass('filter-item--active');
+
+		let cat = $(this).data('filter');
+
+		if (cat == 'all') {
+			$("[data-cat]").removeClass("hide");
+		} else {
+			$("[data-cat]").each(function () {
+				let workCat = $(this).data('cat');
+
+				if (workCat != cat) {
+					$(this).addClass('hide');
+				} else {
+					$(this).removeClass('hide');
+				}
+			});
+		}
+	});
+
+
+
+
+	$(document).ready(function () {
+		$("#lightgallery").lightGallery();
+	});
+
+
+	var a = document.querySelector('#blog-main__sidebar'),
+		b = null,
+		P = 100; // если ноль заменить на число, то блок будет прилипать до того, как верхний край окна браузера дойдёт до верхнего края элемента. Может быть отрицательным числом
+	window.addEventListener('scroll', Ascroll, false);
+	document.body.addEventListener('scroll', Ascroll, false);
+
+	function Ascroll() {
+		if (b == null) {
+			var Sa = getComputedStyle(a, ''),
+				s = '';
+			for (var i = 0; i < Sa.length; i++) {
+				if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
+					s += Sa[i] + ': ' + Sa.getPropertyValue(Sa[i]) + '; '
+				}
+			}
+			b = document.createElement('div');
+			b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
+			a.insertBefore(b, a.firstChild);
+			var l = a.childNodes.length;
+			for (var i = 1; i < l; i++) {
+				b.appendChild(a.childNodes[1]);
+			}
+			a.style.height = b.getBoundingClientRect().height + 'px';
+			a.style.padding = '0';
+			a.style.border = '0';
+		}
+		var Ra = a.getBoundingClientRect(),
+			R = Math.round(Ra.top + b.getBoundingClientRect().height - document.querySelector('.pagination-blog').getBoundingClientRect().top + 40); // селектор блока, при достижении верхнего края которого нужно открепить прилипающий элемент;  Math.round() только для IE; если ноль заменить на число, то блок будет прилипать до того, как нижний край элемента дойдёт до футера
+		if ((Ra.top - P) <= 0) {
+			if ((Ra.top - P) <= R) {
+				b.className = 'stop';
+				b.style.top = -R + 'px';
+			} else {
+				b.className = 'sticky';
+				b.style.top = P + 'px';
+			}
+		} else {
+			b.className = '';
+			b.style.top = '';
+		}
+		window.addEventListener('resize', function () {
+			a.children[0].style.width = getComputedStyle(a, '').width
+		}, false);
+	}
+
+
 
 	$('.timepicker').timepicker({
 		timeFormat: 'h:mm p',
